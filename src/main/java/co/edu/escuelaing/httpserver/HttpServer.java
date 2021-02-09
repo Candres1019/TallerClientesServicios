b0940d7b7bbf4aaf8f081b65032746daccfd8bda
Andres Calderon <mateo.cor.amco@gmail.com>
2021-02-09 15:55:43 -0500

    getPort Function added

--- src/main/java/co/edu/escuelaing/httpserver/HttpServer.java
+++ src/main/java/co/edu/escuelaing/httpserver/HttpServer.java
@@ -15,9 +15,9 @@ public class HttpServer {
     public static void main(String[] args) throws IOException{
         ServerSocket serverSocket = null;
         try{
-            serverSocket = new ServerSocket(36000);
+            serverSocket = new ServerSocket(getPort());
         }catch (IOException e){
-            System.err.println("Could not listen on port : 36000.");
+            System.err.println("Could not listen on port : " + getPort() + ".");
             System.exit(1);
         }
 
@@ -62,6 +62,13 @@ public class HttpServer {
             clientSocket.close();
         }
         serverSocket.close();
+    }
+
+    private static int getPort() {
+        if(System.getenv("PORT") != null){
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 36000;
     }
 
 }
