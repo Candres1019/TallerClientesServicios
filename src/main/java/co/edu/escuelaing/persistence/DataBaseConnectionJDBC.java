fe77ec932e6096ba497488b5fea10fc6af78ef3b
Andres Calderon <mateo.cor.amco@gmail.com>
2021-02-12 10:14:54 -0500

    Connection Manage x4

--- src/main/java/co/edu/escuelaing/persistence/DataBaseConnectionJDBC.java
+++ src/main/java/co/edu/escuelaing/persistence/DataBaseConnectionJDBC.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DataBaseConnectionJDBC {
 
-    private static final String url = "jdbc:postgresql://ec2-34-198-31-223.compute-1.amazonaws.com:5432/d84nik8hj33vv0?ssl=true";
+    private static final String url = "jdbc:postgresql://ec2-34-198-31-223.compute-1.amazonaws.com:5432/d84nik8hj33vv0?sslmode=require";
     private static final String driver = "org.postgresql.Driver";
     private static final String user = "ywxwiezdzzyvcz";
     private static final String pwd = "86163d007692b9ed1146bacdb5e7dd35b3636745d1c259ffac7283c83a24c42b";
